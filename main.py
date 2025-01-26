52241cf052e8347df4e48c62ec90c2e0e670792f
Iva Stanojkovska <79282984+Zlnvrb@users.noreply.github.com>
2025-01-26 13:52:50 +0100

    Add model

--- main.py
+++ main.py
@@ -12,32 +12,36 @@ log = logging.getLogger(__name__)
 
 coloredlogs.install(level='DEBUG')
 
-args = dotdict({
-    'numIters': 5,  # default 1000 -> takes too long
-    'numEps': 60,  # Number of complete self-play games to simulate during a new iteration. default 100
-    'tempThreshold': 15,  # default 15
-    'updateThreshold': 0.1,
+args_predef = dotdict({
+    'numIters': 30,  # default 1000 -> takes too long
+    'numEps': 100,  # Number of complete self-play games to simulate during a new iteration. default 100
+    'tempThreshold': 10,  # default 15
+    'updateThreshold': 0.6,
     # During arena playoff, new neural net will be accepted if threshold or more of games are won. default 0.6
-    'maxlenOfQueue': 512,  # Number of game examples to train the neural networks. default 200000
-    'numMCTSSims': 200,  # Number of games moves for MCTS to simulate. default 25
+    'maxlenOfQueue': 30000,  # Number of game examples to train the neural networks. default 200000
+    'numMCTSSims': 30,  # Number of games moves for MCTS to simulate. default 25
     'arenaCompare': 20,
     # Number of games to play during arena play to determine if new net will be accepted. default 40
-    'cpuct': 2.4,  # default 1
+    'cpuct': 1.2,  # default 1
 
-    'checkpoint': './models/',
-    'load_model': False,
-    'load_folder_file': ('./models/', 'best.pth.tar'),
-    'numItersForTrainExamplesHistory': 100,
+    'checkpoint': './models13/',
+    'load_model': True,
+    'load_folder_file': ('./models12/', 'best.pth.tar'),
+    'numItersForTrainExamplesHistory': 10,
 
 })
 
 
-def main():
+def main(args=None):
+    if args is None:
+        args = args_predef
+    # print(args)
     log.info('Loading %s...', NineMensMorrisGame.__name__)
     g = NineMensMorrisGame()
 
     log.info('Loading %s...', NNetWrapper.__name__)
     nnet = NNetWrapper(g)
+
     if args.load_model:
         log.info('Loading checkpoint "%s/%s"...', args.load_folder_file[0], args.load_folder_file[1])
         nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
